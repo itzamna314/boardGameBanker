1cb5798915af37ceb4dc7e706b8efbd884b429f8
itzamna314 <itsawmna314@gmail.com>
2014-06-08 23:15:26 -0700

    Add readme.  Update db schema.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,15 @@
+# --- Created by Slick DDL
+# To stop Slick DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table `games` (`gameid` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`gamename` VARCHAR(254) NOT NULL);
+create table `players` (`playerid` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`gameid` INTEGER NOT NULL,`userid` INTEGER NOT NULL,`name` VARCHAR(254) NOT NULL,`token` VARCHAR(254) NOT NULL,`score` INTEGER DEFAULT 0 NOT NULL);
+create table `users` (`userid` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`username` VARCHAR(254) NOT NULL,`email` VARCHAR(254) NOT NULL);
+
+# --- !Downs
+
+drop table `games`;
+drop table `players`;
+drop table `users`;
+
